1820295295334ed838ca1257797ba4e633d8eab1
RTX <7odaaliebaideng@gmail.com>
2024-12-06 15:45:06 +0200

    ..

---
+++ lib/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'my_home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MyHomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/splash.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+              child: Image.asset(
+            "assets/Logo.png",
+            width: 0.3 * w,
+            fit: BoxFit.cover,
+          )),
+        ],
+      ),
+    );
+  }
+}
