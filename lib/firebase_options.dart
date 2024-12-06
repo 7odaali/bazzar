d3f3c352b520668217aa605604902bedeb764411
RTX <7odaaliebaideng@gmail.com>
2024-12-06 14:16:46 +0200

    ..

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyBaoz7nOjANU6wBVF6AlT5v_jobb2iyI-o",
+      appId: "1:649847193786:android:4c4c7671aa5eccc6ce8283",
+      messagingSenderId: "649847193786",
+      projectId: "untitledfirebase-1bfca",
+      storageBucket: "untitledfirebase-1bfca.firebasestorage.app",
+    );
+  }
+}
