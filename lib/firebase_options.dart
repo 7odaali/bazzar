daca6173dbb19cd2f110f3865f998e011455173e
RTX <7odaaliebaideng@gmail.com>
2024-12-07 02:54:59 +0200

    OnboardingScreen...firebase

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,25 +1,8 @@
-/*
-
-import 'package:firebase_core/firebase_core.dart';
-
-class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    return const FirebaseOptions(
-      apiKey: "AIzaSyDnU8U_Gv64o9Ik-AeXPY_mXZwIFgRe7mk",
-      appId: "1:142037389077:android:7e7f243581c99f7ae7231c",
-      messagingSenderId: "142037389077",
-      projectId: "pazzar-36917",
-      storageBucket: "pazzar-36917.firebasestorage.app",
-    );
-  }
-}*/
-// lib/firebase_options.dart
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    return android; // اختر منصة التطبيق
+    return android;
   }
 
   static const FirebaseOptions android = FirebaseOptions(
