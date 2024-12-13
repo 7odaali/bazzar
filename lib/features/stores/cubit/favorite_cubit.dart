c26380627157f68c908cf17a6a6f6b5cd7e43649
RTX <7odaaliebaideng@gmail.com>
2024-12-14 00:59:11 +0200

    Favoritecubit

--- lib/features/stores/cubit/favorite_cubit.dart
+++ lib/features/stores/cubit/favorite_cubit.dart
@@ -1,17 +1,52 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+/*
+
+import 'package:bloc/bloc.dart';
 
 class FavoritesCubit extends Cubit<List<Map<String, dynamic>>> {
   FavoritesCubit() : super([]);
 
   void addProduct(Map<String, dynamic> product) {
     final updatedFavorites = List<Map<String, dynamic>>.from(state);
-    updatedFavorites.add(product);
+
+    bool productExists =
+        updatedFavorites.any((item) => item['name'] == product['name']);
+
+    if (!productExists) {
+      updatedFavorites.add(product);
+      emit(updatedFavorites);
+    }
+  }
+
+  void removeProduct(Map<String, dynamic> product) {
+    final updatedFavorites = List<Map<String, dynamic>>.from(state);
+    updatedFavorites.removeWhere((item) => item['name'] == product['name']);
+    emit(updatedFavorites);
+  }
+}
+*/
+import 'package:bloc/bloc.dart';
+
+class FavoritesCubit extends Cubit<List<Map<String, dynamic>>> {
+  FavoritesCubit() : super([]);
+
+  void updateFavorite(Map<String, dynamic> product) {
+    final updatedFavorites = List<Map<String, dynamic>>.from(state);
+
+    bool productExists =
+        updatedFavorites.any((item) => item['name'] == product['name']);
+
+    if (productExists) {
+      updatedFavorites.removeWhere((item) => item['name'] == product['name']);
+    } else {
+      updatedFavorites.add(product);
+    }
+
     emit(updatedFavorites);
   }
 
   void removeProduct(Map<String, dynamic> product) {
     final updatedFavorites = List<Map<String, dynamic>>.from(state);
-    updatedFavorites.remove(product);
+    updatedFavorites.removeWhere((item) => item['name'] == product['name']);
     emit(updatedFavorites);
   }
 }
