831ef5bdf25d28dd6b863efd37bd8793977a2db4
RTX <7odaaliebaideng@gmail.com>
2024-12-17 00:09:20 +0200

    Correct BottomNavigationBar

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import '../../features/card/cart_screen.dart';
 import '../../features/home/home_screen.dart';
@@ -58,6 +59,87 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
           BottomNavigationBarItem(
             icon: Icon(Icons.menu),
             label: 'Menu',
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../features/card/cart_screen.dart';
+import '../../features/home/home_screen.dart';
+import '../../features/stores/store_screen.dart';
+
+class BottomNavigationBarScreen extends StatefulWidget {
+  const BottomNavigationBarScreen({super.key});
+
+  @override
+  State<BottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarScreenState();
+}
+
+class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const StoreScreen(),
+    const CartScreen(),
+    const Center(child: Text('My Account Screen')),
+    const Center(child: Text('Menu Screen')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.r),
+          topRight: Radius.circular(25.r),
+        ),
+        child: Container(
+          color: const Color(0xFF0F1325),
+          padding: EdgeInsets.only(top: 10.h, right: 15.w, left: 15.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildNavItem(Icons.home, 'Home', 0),
+              _buildNavItem(Icons.store, 'Stores', 1),
+              _buildNavItem(Icons.shopping_bag_outlined, 'Cart', 2),
+              _buildNavItem(Icons.account_circle, 'My Account', 3),
+              _buildNavItem(Icons.menu, 'Menu', 4),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, int index) {
+    final isSelected = _currentIndex == index;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: isSelected ? Colors.yellow : Colors.white,
+          ),
+          SizedBox(height: 5.h),
+          Text(
+            label,
+            style: TextStyle(
+              color: isSelected ? Colors.yellow : Colors.white,
+              fontSize: 12.sp,
+            ),
           ),
         ],
       ),
